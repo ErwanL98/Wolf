b29c23b2831b6dfe7f7e7b1f63a4a23b39c8e3ed
ele-cren <ele-cren@student.42.fr>
2017-04-25 09:43:26 +0200

    uncompiled

--- SDL2/SDL2/build/SDL_malloc.d
+++
@@ -1,7 +0,0 @@
-build/SDL_malloc.lo: \
-  /Users/ele-cren/Projetcts/Current/wolf/SDL2/SDL2/src/stdlib/SDL_malloc.c \
-  /Users/ele-cren/Projetcts/Current/wolf/SDL2/SDL2/src/stdlib/../SDL_internal.h \
-  /Users/ele-cren/Projetcts/Current/wolf/SDL2/SDL2/src/stdlib/../dynapi/SDL_dynapi.h \
-  /Users/ele-cren/Projetcts/Current/wolf/SDL2/SDL2/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h include/SDL_stdinc.h
