000eb6f88a5ec1f47ec09d644685aee602e5609d
ele-cren <ele-cren@student.42.fr>
2017-04-25 12:57:23 +0200

    correct

--- SDL2/SDL2/build/.libs/SDL_egl.d
+++
@@ -1,7 +0,0 @@
-build/SDL_egl.lo: \
-  /Users/ele-cren/Projetcts/Current/wolf/SDL2/SDL2/src/video/SDL_egl.c \
-  /Users/ele-cren/Projetcts/Current/wolf/SDL2/SDL2/src/video/../SDL_internal.h \
-  /Users/ele-cren/Projetcts/Current/wolf/SDL2/SDL2/src/video/../dynapi/SDL_dynapi.h \
-  /Users/ele-cren/Projetcts/Current/wolf/SDL2/SDL2/src/video/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h
